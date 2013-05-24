bdb8262bfd9f1885963c5bdb25c5c87ce8acc044
Shayne Studdard <sstuddard@gmail.com>
2013-05-23 20:59:04 -0500

    Command line args in, running from command line

--- src/simple_kmeans/core.clj
+++ src/simple_kmeans/core.clj
@@ -4,6 +4,7 @@
         [simple-kmeans.sparsevector]
         [simple-kmeans.clustering]
         [simple-kmeans.geometry]
+        [clojure.tools.cli :only [cli]]
         [clojure.set])
   (:require clojure.java.io))
 
@@ -15,10 +16,23 @@
   "Run k-means clustering on documents from a line-delimited file. The first token of a line 
     is the identifier for the document."
   [& args]
-  (let [filepath (first args)
-        data (get-lines filepath)
-        documents (map tokenize data)
-        vocabulary (get-vocabulary documents)
-        vectors (map #(get-term-vector % (vocab-term-lookup vocabulary)) documents)]
-    (println "Found" (count data) "documents.")
-    (println "Vectors: " vectors)))
+  (let [opts (cli args
+               ["-k" "--clusters" "Specify number of clusters" :parse-fn #(Integer. %)] 
+               ["-f" "--file" "The hostname"]
+               ["-n" "--randomruns" "Do runs n times" :parse-fn #(Integer. %) :default 1]
+               ["-m" "--iterations" "The max iterations for convergence" :parse-fn #(Integer. %) :default 10])]
+
+    (let [filepath ((first opts) :file)
+          data (get-lines filepath)
+          documents (map tokenize data)
+          vocabulary (get-vocabulary documents)
+          vectors (map #(get-term-vector % (vocab-term-lookup vocabulary)) documents)
+          runs ((first opts) :randomruns)
+          k ((first opts) :clusters)
+          convergence-iterations ((first opts) :iterations)
+          distance-function euclidean-distance]
+      (def centroids (take runs (repeat (random-centroids k vectors))))
+      (def result (optimize-cluster centroids vectors distance-function convergence-iterations))
+      (println "Found" (count data) "documents.")
+      (println "Testing" runs "times")
+      (println "Results (error:" (cluster-error result distance-function) "):" result))))
